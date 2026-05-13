e67d0556db6b67f0f23371bf4dde8eeffc01abe0
Evan Thompson <evanpthompson@gmail.com>
2026-05-13 01:59:33 -0500

    Update test

--- server/landfall_server/test/integration/test_tools/serverpod_test_tools.dart
+++ server/landfall_server/test/integration/test_tools/serverpod_test_tools.dart
@@ -1138,6 +1138,36 @@ class _CompanionEndpoint {
       }
     });
   }
+
+  _i3.Future<String> getCompanionBaseUrl(
+    _i1.TestSessionBuilder sessionBuilder,
+  ) async {
+    return _i1.callAwaitableFunctionAndHandleExceptions(() async {
+      var _localUniqueSession =
+          (sessionBuilder as _i1.InternalTestSessionBuilder).internalBuild(
+            endpoint: 'companion',
+            method: 'getCompanionBaseUrl',
+          );
+      try {
+        var _localCallContext = await _endpointDispatch.getMethodCallContext(
+          createSessionCallback: (_) => _localUniqueSession,
+          endpointPath: 'companion',
+          methodName: 'getCompanionBaseUrl',
+          parameters: _i1.testObjectToJson({}),
+          serializationManager: _serializationManager,
+        );
+        var _localReturnValue =
+            await (_localCallContext.method.call(
+                  _localUniqueSession,
+                  _localCallContext.arguments,
+                )
+                as _i3.Future<String>);
+        return _localReturnValue;
+      } finally {
+        await _localUniqueSession.close();
+      }
+    });
+  }
 }
 
 class _GreetingEndpoint {
